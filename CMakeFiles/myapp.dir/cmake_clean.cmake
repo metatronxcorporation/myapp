ce844e812194b89792bb196eb6c82d6e88a99f5f
Metatron X Corporation <corporationmetatron@gmail.com>
2023-03-03 10:28:46 +0900

    Initial Commit

---
+++ CMakeFiles/myapp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/myapp.dir/source/main.cpp.o"
+  "CMakeFiles/myapp.dir/source/mainwindow.cpp.o"
+  "myapp.pdb"
+  "myapp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/myapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
